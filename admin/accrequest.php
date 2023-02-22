0ff4d2aaf556d9b925fdb09c177055cf87a0e098
mdna99 <82161441+mdna99@users.noreply.github.com>
2023-02-22 14:02:14 +0700

    fungsi acc 1

--- admin/accrequest.php
+++ admin/accrequest.php
@@ -2,93 +2,37 @@
 // include_once("../config/config.php");
 include_once("../config/functions.php");
 
-	$id_peminjaman = $_GET["id"];
-	$cekpeminjaman = squery("SELECT * FROM peminjaman WHERE id_peminjaman = $id_peminjaman");
-	// $this->db->get_where('peminjaman', ['id_peminjaman' => $id_peminjaman])->row_array();
+$id_peminjaman = $_GET["id"];
+$cekpeminjaman = squery("SELECT * FROM `peminjaman` WHERE `id_peminjaman` = '$id_peminjaman'");
+// $this->db->get_where('peminjaman', ['id_peminjaman' => $id_peminjaman])->row_array();
 
-	$nowtime = strtotime(date('H:i:s')) + strtotime(date('Y-m-d'));
+$nowtime = strtotime(date('H:i:s')) + strtotime(date('Y-m-d'));
 
-	$dbstart = strtotime($cekpeminjaman[0]['jam_mulai']) + strtotime($cekpeminjaman[0]['tanggal']);
-	$dbend = strtotime($cekpeminjaman[0]['jam_berakhir']) + strtotime($cekpeminjaman[0]['tanggal']);
+$dbstart = strtotime($cekpeminjaman[0]['jam_mulai']) + strtotime($cekpeminjaman[0]['tanggal']);
+$dbend = strtotime($cekpeminjaman[0]['jam_berakhir']) + strtotime($cekpeminjaman[0]['tanggal']);
 
-	if ($nowtime >= $dbstart and $nowtime <= $dbend) {
-		$cekjadwal = squery("SELECT * FROM `jadwal` INNER JOIN peminjaman
-		WHERE jadwal.id_peminjaman=peminjaman.id_peminjaman
-		AND status_jadwal=1");
+if ($nowtime >= $dbstart and $nowtime <= $dbend) {
+	$cekjadwal = squery("SELECT * FROM `jadwal` INNER JOIN `peminjaman`
+								WHERE `jadwal`.`id_peminjaman`=`peminjaman`.`id_peminjaman`
+							AND `status_jadwal`=1");
 
-		if ($cekjadwal) {
-			echo "
+	if ($cekjadwal) {
+		echo "
             <script>
                 alert('Gagal diterima, jadwal bentrok!');
                 document.location.href= 'pengajuan.php';
             </script>
             ";
-		} else {
-			query("UPDATE `peminjaman` SET
+	} else {
+		query("UPDATE `peminjaman` SET
 						`status_peminjaman` = 1
 					WHERE
 						`id_peminjaman` = '$id_peminjaman'
 				");
-			query("INSERT INTO `jadwal` ('id_jadwal', 'id_peminjaman', 'status_jadwal') 
+		query("INSERT INTO `jadwal` ('id_jadwal', 'id_peminjaman', 'status_jadwal') 
 						VALUES
-					(NULL, '$id_peminjaman', 1)");
-			// $this->db->update('peminjaman', array('status_peminjaman' => 1), array('id_peminjaman' => $id_peminjaman));
-			// $this->db->insert('jadwal', array(
-			// 	'id_jadwal' => null,
-			// 	'id_peminjaman' => $id_peminjaman,
-			// 	'status_jadwal' => 1
-			// ));
-			echo "
-            <script>
-                alert('Request diterima!');
-                document.location.href= 'pengajuan.php';
-            </script>
-            ";
-			// $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Request diterima!</div>');
-			// redirect('admin/request');
-		}
-	} elseif ($nowtime < $dbstart) {
-		$cekjadwal = squery("SELECT * FROM `jadwal` INNER JOIN peminjaman
-		WHERE `jadwal`.`id_peminjaman`=`peminjaman`.`id_peminjaman`
-		AND `status_jadwal`=2");
-
-		$dbone = strtotime($cekjadwal['jam_mulai']) + strtotime($cekjadwal['jam_ berakhir']) + strtotime($cekjadwal['tanggal']);
-
-		$dbtwo = strtotime($cekpeminjaman[0]['jam_mulai']) + strtotime($cekpeminjaman[0]['jam_ berakhir']) + strtotime($cekpeminjaman[0]['tanggal']);
-
-		if ($dbone == $dbtwo) {
-			echo "
-            <script>
-                alert('Gagal diterima, jadwal bentrok!');
-                document.location.href= 'pengajuan.php';
-            </script>
-            ";
-			// $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Gagal diterima, jadwal bentrok!</div>');
-			// redirect('admin/request');
-		} else {
-			query("UPDATE `peminjaman` SET `status_peminjaman` = 2 WHERE `id_peminjaman` = '$id_peminjaman'");
-			query("INSERT INTO `jadwal` ('id_jadwal', 'id_peminjaman', 'status_jadwal')
-						VALUES
-					(NULL, '$id_peminjaman', 2)");
-			// $this->db->update('peminjaman', array('status_peminjaman' => 2), array('id_peminjaman' => $id_peminjaman));
-			// $this->db->insert('jadwal', array(
-			// 	'id_jadwal' => null,
-			// 	'id_peminjaman' => $id_peminjaman,
-			// 	'status_jadwal' => 2
-			// ));
-			echo "
-            <script>
-                alert('Request diterima!');
-                document.location.href= 'pengajuan.php';
-            </script>
-            ";
-			// $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Request diterima!</div>');
-			// redirect('admin/request');
-		}
-	} else {
-		query("INSERT INTO `jadwal` ('id_jadwal', 'id_peminjaman', 'status_jadwal')
-				VALUES
-			(NULL, '$id_peminjaman', 1)");
+					(NULL, '$id_peminjaman', '1')");
+		// $this->db->update('peminjaman', array('status_peminjaman' => 1), array('id_peminjaman' => $id_peminjaman));
 		// $this->db->insert('jadwal', array(
 		// 	'id_jadwal' => null,
 		// 	'id_peminjaman' => $id_peminjaman,
@@ -103,3 +47,59 @@ include_once("../config/functions.php");
 		// $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Request diterima!</div>');
 		// redirect('admin/request');
 	}
+} elseif ($nowtime < $dbstart) {
+	$cekjadwal = squery("SELECT * FROM `jadwal` INNER JOIN `peminjaman`
+		WHERE `jadwal`.`id_peminjaman`=`peminjaman`.`id_peminjaman`
+		AND `status_jadwal`=2");
+
+	$dbone = strtotime($cekjadwal['jam_mulai']) + strtotime($cekjadwal['jam_ berakhir']) + strtotime($cekjadwal['tanggal']);
+
+	$dbtwo = strtotime($cekpeminjaman[0]['jam_mulai']) + strtotime($cekpeminjaman[0]['jam_ berakhir']) + strtotime($cekpeminjaman[0]['tanggal']);
+
+	if ($dbone == $dbtwo) {
+		echo "
+            <script>
+                alert('Gagal diterima, jadwal bentrok!');
+                document.location.href= 'pengajuan.php';
+            </script>
+            ";
+		// $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Gagal diterima, jadwal bentrok!</div>');
+		// redirect('admin/request');
+	} else {
+		query("UPDATE `peminjaman` SET `status_peminjaman` = 2 WHERE `id_peminjaman` = '$id_peminjaman'");
+		query("INSERT INTO `jadwal` ('id_jadwal', 'id_peminjaman', 'status_jadwal')
+						VALUES
+					(NULL, '$id_peminjaman', 2)");
+		// $this->db->update('peminjaman', array('status_peminjaman' => 2), array('id_peminjaman' => $id_peminjaman));
+		// $this->db->insert('jadwal', array(
+		// 	'id_jadwal' => null,
+		// 	'id_peminjaman' => $id_peminjaman,
+		// 	'status_jadwal' => 2
+		// ));
+		echo "
+            <script>
+                alert('Request diterima!');
+                document.location.href= 'pengajuan.php';
+            </script>
+            ";
+		// $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Request diterima!</div>');
+		// redirect('admin/request');
+	}
+} else {
+	query("INSERT INTO `jadwal` ('id_jadwal', 'id_peminjaman', 'status_jadwal')
+				VALUES
+			(NULL, '$id_peminjaman', 1)");
+	// $this->db->insert('jadwal', array(
+	// 	'id_jadwal' => null,
+	// 	'id_peminjaman' => $id_peminjaman,
+	// 	'status_jadwal' => 1
+	// ));
+	echo "
+            <script>
+                alert('Request diterima!');
+                document.location.href= 'pengajuan.php';
+            </script>
+            ";
+	// $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Request diterima!</div>');
+	// redirect('admin/request');
+}
--- template/sidebar.php
+++ template/sidebar.php
@@ -1,56 +1,56 @@
 <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.php">
-                <div class="sidebar-brand- icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">DiOR Admin</div>
-            </a>
-            
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="dashboard.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.php">
+        <div class="sidebar-brand- icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">DiOR Admin</div>
+    </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="dashboard.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link" href="pengajuan.php">
-                    <i class="fas fa-fw fa-bookmark"></i>
-                    <span>Pengajuan</span>
-                </a>
-            </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link" href="jadwal.php">
-                    <i class="fas fa-fw fa-calendar"></i>
-                    <span>Jadwal Peminjaman</span>
-                </a>
-            </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="pengajuan.php">
+            <i class="fas fa-fw fa-bookmark"></i>
+            <span>Pengajuan</span>
+        </a>
+    </li>
 
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link" href="datapeminjam.php">
-                    <i class="fas fa-fw fa-users"></i>
-                    <span>Kelola Data Pengguna</span>
-                </a>
-            </li>
+    <!-- Nav Item - Utilities Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="jadwal.php">
+            <i class="fas fa-fw fa-calendar"></i>
+            <span>Jadwal Peminjaman</span>
+        </a>
+    </li>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
+    <!-- Nav Item - Utilities Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="datapeminjam.php">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Kelola Data Pengguna</span>
+        </a>
+    </li>
 
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-        </ul>
-        <!-- End of Sidebar -->
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
+<!-- End of Sidebar -->
